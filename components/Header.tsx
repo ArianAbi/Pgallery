eb8bba524e1aa3e37362fad3ee90366de23cf038
arian <kapqworks@gmail.com>
2023-02-02 21:59:46 +0330

    ADD basic header

---
+++ components/Header.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react"
+import Router, { useRouter } from "next/router"
+
+export const Header = () => {
+
+    const loginRoute = "/login"
+    const signupRoute = "/signup"
+    const router = useRouter();
+
+    if (router.pathname === loginRoute || router.pathname === signupRoute) {
+        return
+    }
+    return (
+        <>
+            <header className="grid grid-cols-3 w-screen bg-slate-200 text-center p-3">
+                <div className="text-left">
+                    Menu
+                </div>
+
+                <div className="text-xl font-bold">
+                    Logo
+                </div>
+
+                <div className="text-right">
+                    <a href={loginRoute}>Profile</a>
+                </div>
+            </header>
+        </>
+    )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { Header } from '@/components/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const dbUrl: any = process.env.dbUrl;
+  const dbSecret: any = process.env.dbSecret;
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Header />
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  )
 }
