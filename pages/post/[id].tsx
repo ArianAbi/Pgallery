ec0b90bdea00ea0c6b586e2e99adbfebf6f8fc1a
arian <kapqworks@gmail.com>
2023-02-20 02:27:01 +0330

    UPDATE post page

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -21,6 +21,9 @@ export default function PostPage() {
 
     const [post, setPost] = useState<data>()
     const [imageUrl, setImageUrl] = useState<string>()
+    const [creatorUsername, setCreatorUsername] = useState<string>();
+
+    const [loading, setLoading] = useState(true);
 
     const { id } = router.query
 
@@ -36,38 +39,87 @@ export default function PostPage() {
             }
 
             const { data: _imageUrl } = await supabaseClient.storage.from('posts').getPublicUrl(data[0].image_path);
-
             setImageUrl(_imageUrl.publicUrl)
 
+            const { data: creatorUsername, error: creatorUsernameErr } = await supabaseClient.from('users').select('user_name').eq('user_id', data[0].creator_id)
+
+            setCreatorUsername(creatorUsername && creatorUsername[0].user_name)
             setPost(data[0])
+            setLoading(false)
         })()
     }, [id])
 
     return (
         <>
-            <div className="text-white">
-                {/* image */}
-                {post && imageUrl &&
-
-                    <Image
-                        alt={post.description}
-                        src={imageUrl}
-                        width={200}
-                        height={200}
-                    />
-
-                }
-                {/* title */}
-                <h1>{post?.title}</h1>
-
-                {/* date */}
-                <span>{dayjs(post?.date).format('YY/MM/DD')}</span>
-
-                {/* description */}
-                <p>{post?.description}</p>
+            {loading &&
+                <div className="flex flex-col gap-4">
+                    {/* image placeholder */}
+                    <div className="w-full aspect-video bg-slate-400 animate-pulse"></div>
 
 
-            </div>
+                    <div className="flex flex-col gap-4 px-4">
+                        {/* title placeholder */}
+                        <span className="w-[220px] h-8 bg-slate-400 rounded-md animate-pulse"></span>
+
+                        {/* profile placeholder */}
+                        <div className="flex gap-2">
+                            <div className="w-12 aspect-square rounded-full bg-slate-400 animate-pulse"></div>
+
+                            <div className="w-[100px] h-6 rounded-md bg-slate-400 animate-pulse"></div>
+                        </div>
+
+                        {/* description placeholder */}
+                        <span className="w-[320px] h-6 bg-slate-400 rounded-md animate-pulse"></span>
+                        <span className="w-[320px] h-6 bg-slate-400 rounded-md animate-pulse"></span>
+                        <span className="w-[320px] h-6 bg-slate-400 rounded-md animate-pulse"></span>
+                    </div>
+                </div>
+            }
+
+            {!loading &&
+
+                <div className="flex flex-col gap-4 text-white">
+                    {/* image */}
+                    {post && imageUrl &&
+                        <Image
+                            className="w-full max-h-[50vh] aspect-auto object-contain bg-stone-900"
+                            placeholder="empty"
+                            alt={post.description}
+                            src={imageUrl}
+                            width={200}
+                            height={200}
+                        />
+                    }
+
+                    <div className="px-4 flex flex-col gap-4">
+                        {/* title */}
+                        <h1 className="text-xl font-semibold">
+                            {post?.title}
+                        </h1>
+
+                        {/* profile */}
+                        <div>
+                            <div className="flex gap-2">
+                                <div className="w-12 aspect-square rounded-full bg-slate-400"></div>
+
+                                <div className="mt-1">{creatorUsername}</div>
+                            </div>
+                        </div>
+
+                        {/* date */}
+                        <span className="text-sm italic flex gap-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
+                            </svg>
+
+                            {dayjs(post?.date).format('YY/MM/DD')}
+                        </span>
+
+                        {/* description */}
+                        <p>{post?.description}</p>
+                    </div>
+                </div>
+            }
 
             <MobileNav />
         </>
